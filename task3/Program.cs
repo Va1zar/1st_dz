0543ce39990b41ec0689c8041823f87dfa35eceb
Evgeniy <s.h.a.r.k.112@mail.ru>
2022-11-29 20:01:47 +0700

    add solution 3rd task

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
 Console.WriteLine("Введите число = ");
-double A = double.Parse(Console.ReadLine()!);
+int A = int.Parse(Console.ReadLine()!);
 
 if( A % 2 == 0 )
 {
